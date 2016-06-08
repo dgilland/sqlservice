222320f44e1a8deb3601418dfb3a0053cb29a61e
Derrick Gilland <dgilland@gmail.com>
2016-06-07 21:58:43 -0400

    Bump package dev version.

--- sqlservice/__pkg__.py
+++ sqlservice/__pkg__.py
@@ -5,7 +5,7 @@
 __package_name__ = 'sqlservice'
 __description__ = 'The missing SQLAlchemy ORM service layer'
 __url__ = 'https://github.com/dgilland/sqlservice'
-__version__ = '0.1.0'
+__version__ = '0.2.0-dev'
 __author__ = 'Derrick Gilland'
 __email__ = 'dgilland@gmail.com'
 __license__ = 'MIT License'
