562e373c1231d6d033abcf3c1383bd65ad2bb5c6
Derrick Gilland <dgilland@gmail.com>
2020-10-29 11:07:09 -0400

    Set package version in setup.cfg.

--- setup.py
+++ setup.py
@@ -1,20 +1,6 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
-
-
-package_dir = 'src'
-packages = find_packages(package_dir)
-
-version = {}
-with open('{}/{}/__version__.py'.format(package_dir, packages[0])) as f:
-    exec(f.read(), version)
+from setuptools import setup
 
 
-setup(
-    version=version['__version__'],
-    package_dir={'': package_dir},
-    packages=packages,
-    include_package_data=True
-)
+setup()
--- src/sqlservice/__version__.py
+++ src/sqlservice/__version__.py
@@ -1,3 +1,10 @@
 """Project version information."""
 
-__version__ = "1.2.1"
+from pkg_resources import DistributionNotFound, get_distribution
+
+
+try:
+    __version__ = get_distribution(__name__.split(".")[0]).version
+except DistributionNotFound:  # pragma: no cover
+    # Package is not installed.
+    __version__ = None
